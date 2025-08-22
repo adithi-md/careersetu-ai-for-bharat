2902d5946b3afff685b113432a465bde2a244996
adithi m d <adithidatta16@gmail.com>
2025-08-23 03:17:58 +0530

    Final project submission

---
+++ FlutterProjects/acadflow/android/app/src/main/kotlin/com/example/acadflow/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.acadflow
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ FlutterProjects/acadflow/lib/dashboard_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('📊 Dashboard'),
+      ),
+      body: Center(
+        child: Text(
+          'Welcome to Your Study Tracker Dashboard!',
+          style: TextStyle(fontSize: 18),
+        ),
+      ),
+    );
+  }
+}
